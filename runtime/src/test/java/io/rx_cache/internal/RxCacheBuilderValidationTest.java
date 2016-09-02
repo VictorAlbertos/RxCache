8464c83d36b42ee03812e3fe188d2bf611df0d6d
Victor <victor@refineriaweb.com>
2016-09-02 16:34:09 +0200

    Reformat style and add license to class RxCacheBuilderValidationTest

--- runtime/src/test/java/io/rx_cache/internal/RxCacheBuilderValidationTest.java
+++ runtime/src/test/java/io/rx_cache/internal/RxCacheBuilderValidationTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 Victor Albertos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.rx_cache.internal;
 
 import org.junit.Rule;
@@ -12,38 +28,38 @@ import java.security.InvalidParameterException;
  */
 public class RxCacheBuilderValidationTest {
 
-    @Rule
-    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    @Test(expected = InvalidParameterException.class)
-    public void Cache_Directory_Null() {
-        new RxCache.Builder()
-            .persistence(null, Jolyglot$.newInstance());
-    }
+  @Test(expected = InvalidParameterException.class)
+  public void Cache_Directory_Null() {
+    new RxCache.Builder()
+        .persistence(null, Jolyglot$.newInstance());
+  }
 
-    @Test(expected = InvalidParameterException.class)
-    public void Jolyglot_Null() {
-        new RxCache.Builder()
-                .persistence(temporaryFolder.getRoot(), null);
-    }
+  @Test(expected = InvalidParameterException.class)
+  public void Jolyglot_Null() {
+    new RxCache.Builder()
+        .persistence(temporaryFolder.getRoot(), null);
+  }
 
-    @Test(expected = InvalidParameterException.class)
-    public void Cache_Directory_Not_Exist() {
-        File cacheDir = new File(temporaryFolder.getRoot(), "non_existent_folder");
-        new RxCache.Builder()
-                .persistence(cacheDir, Jolyglot$.newInstance());
-    }
+  @Test(expected = InvalidParameterException.class)
+  public void Cache_Directory_Not_Exist() {
+    File cacheDir = new File(temporaryFolder.getRoot(), "non_existent_folder");
+    new RxCache.Builder()
+        .persistence(cacheDir, Jolyglot$.newInstance());
+  }
 
-    @Test(expected = InvalidParameterException.class)
-    public void Cache_Directory_Not_Writable() {
-        File cacheDir = new File(temporaryFolder.getRoot(), "non_existent_folder");
-        if (!cacheDir.mkdirs()) {
-            throw new IllegalStateException("Cannot create temporary directory");
-        }
-        if (!cacheDir.setWritable(false, false)) {
-            throw new IllegalStateException("Cannot modify permissions");
-        }
-        new RxCache.Builder()
-                .persistence(cacheDir, Jolyglot$.newInstance());
+  @Test(expected = InvalidParameterException.class)
+  public void Cache_Directory_Not_Writable() {
+    File cacheDir = new File(temporaryFolder.getRoot(), "non_existent_folder");
+    if (!cacheDir.mkdirs()) {
+      throw new IllegalStateException("Cannot create temporary directory");
     }
+    if (!cacheDir.setWritable(false, false)) {
+      throw new IllegalStateException("Cannot modify permissions");
+    }
+    new RxCache.Builder()
+        .persistence(cacheDir, Jolyglot$.newInstance());
+  }
 }
